99008f02f16a78a1c608d60539186f3b75a512a8
AgilRasyidH <agil.rasyid8@gmail.com>
2022-01-03 08:51:52 +0700

    Menambahkan fungsionalitas mult

--- src/com/oop/git/Aritmatika.java
+++ src/com/oop/git/Aritmatika.java
@@ -1,5 +1,20 @@
 package com.oop.git;
 
 public class Aritmatika {
+	//Developer 1
+	public double add(int a, int b) {
+		return a + b;
+	}
 	
+	//Developer 2
+	public double sub(int a, int b) {
+		return a - b;
+	}
+	
+	//Developer 2 (mult)
+	public double mult (int a, int b) {
+		return a * b;
+	//Developer 3 (div)
+	
+	}
 }
